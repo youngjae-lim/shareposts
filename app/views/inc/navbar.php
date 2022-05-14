25d5e44c92ed3053023462a686d0b616dbe15c16
Youngjae Lim <limyoungjae@gmail.com>
2022-05-13 23:27:06 -0400

    Add register & login form views

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,23 +1,23 @@
-<nav class="navbar navbar-dark bg-dark" aria-label="First navbar example">
-    <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="First navbar example">
+    <div class="container">
     <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarsExample01">
-        <ul class="navbar-nav me-auto mb-2">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
+            <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
           </li>
           <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
           </li>
         </ul>
         
-        <ul class="navbar-nav ml-auto mb-2">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>/users/register">Register</a>
+            <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>/users/register">Register</a>
           </li>
           <li class="nav-item">
           <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Login</a>
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -3,13 +3,8 @@
     <div class="col-md-6 mx-auto">
       <div class="card card-body bg-light mt-5">
         <h2>Create An Account</h2> 
-        <p>Please fill out this form to register with us.</p>
-        <form action="<?php echo URLROOT; ?>/users/register" method="POST">
-          <div class="form-group mb-3">
-            <label for="name" class="form-label">Name: <sup>*</sup></label> 
-            <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
-            <span class="invalid-feedback"><?php echo $data["name_err"]; ?></span>
-          </div>
+        <p>Please fill in your credentials to log in.</p>
+        <form action="<?php echo URLROOT; ?>/users/login" method="POST">
           <div class="form-group mb-3">
             <label for="email" class="form-label">Email: <sup>*</sup></label> 
             <input type="text" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
@@ -20,15 +15,10 @@
             <input type="text" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
             <span class="invalid-feedback"><?php echo $data["password_err"]; ?></span>
           </div>
-          <div class="form-group mb-3">
-            <label for="confirm_password" class="form-label">Confirm Password: <sup>*</sup></label> 
-            <input type="text" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
-            <span class="invalid-feedback"><?php echo $data["confirm_password_err"]; ?></span>
-          </div>
 
           <div class="d-grid gap-2 d-md-block col-6">
-            <input type="submit" value="Register" class="btn btn-success"> 
-            <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light">Have an account? Login</a>
+            <input type="submit" value="Login" class="btn btn-success"> 
+            <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-light">Have no account? Register</a>
           </div>
         </form>
       </div>

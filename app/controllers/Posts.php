3538edee2b774189f16ca94b50f739e43ba06532
Youngjae Lim <limyoungjae@gmail.com>
2022-05-16 14:48:13 -0400

    Make /posts a protected route from non-logged-in users

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -2,6 +2,13 @@
 
 class Posts extends Controller
 {
+    public function __construct()
+    {
+        // Protect the /posts route from non-logged-in users
+        if (!isLoggedIn()) {
+            redirect('users/login');
+        }
+    }
     public function index()
     {
         $data = [];
--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -162,7 +162,7 @@ class Users extends Controller
         $_SESSION['user_id'] = $user->id;
         $_SESSION['user_email'] = $user->email;
         $_SESSION['user_name'] = $user->name;
-        redirect('pages/index');
+        redirect('posts');
     }
 
     public function logout()
@@ -172,14 +172,5 @@ class Users extends Controller
         unset($_SESSION['user_name']);
         session_destroy();
         redirect('users/login');
-    }
-
-    public function isLoggedIn()
-    {
-        if (isset($_SESSION['user_id'])) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }

19421448e17f9280897659c596fc99b277e0cda0
Youngjae Lim <limyoungjae@gmail.com>
2022-05-15 22:43:17 -0400

    Add custom flash message

---
+++ app/helpers/session_helper.php
@@ -0,0 +1,30 @@
+<?php
+
+  session_start();
+
+  // Flash message helper
+  // Usage - flash('register_success', 'Your are now registered');
+  // Display in View -  echo flash('register_success');
+function flash($name = '', $message = '', $class = 'alert alert-success')
+{
+    if (!empty($name)) {
+        if (!empty($message)) {
+            if (!empty($_SESSION[$name])) {
+                unset($_SESSION[$name]);
+            }
+
+            if (!empty($_SESSION[$name . '_class'])) {
+                unset($_SESSION[$name . '_class']);
+            }
+            // Set new session values
+            $_SESSION[$name] = $message;
+            $_SESSION[$name . '_class'] = $class;
+        } elseif (!empty($_SESSION[$name])) {
+            echo '<div class="' . $_SESSION[$name . '_class'] . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
+
+            // Unset the existing session values
+            unset($_SESSION[$name]);
+            unset($_SESSION[$name . '_class']);
+        }
+    }
+}

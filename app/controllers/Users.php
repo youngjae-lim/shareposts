1a97e378ff4f7fa540e4090f668e1d394a7ab8b2
Youngjae Lim <limyoungjae@gmail.com>
2022-05-16 06:29:14 -0400

    Add user login functionality

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -117,10 +117,28 @@ class Users extends Controller
                 $data['password_err'] = 'Please enter password';
             }
 
+            // Check for user/email
+            if ($this->userModel->findUserByEmail($data['email'])) {
+              // User found
+            } else {
+              // User not found
+                $data['email_err'] = 'No user found';
+            }
+
             // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['password_err'])) {
                 // Validated
-                die('SUCCESS');
+                // Check and set logged in user
+                $loggedInUser = $this ->userModel->login($data['email'], $data['password']);
+
+                if ($loggedInUser) {
+                  // Create session
+                    die('SUCCESS');
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+
+                    $this->view('users/login', $data);
+                }
             } else {
                 // Load view with Errors
                 $this->view('users/login', $data);
